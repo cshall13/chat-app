db3c4608af26a284cf163eb1f1b050584ce47fbf
Shane Hall <cyrus.s.hall84@gmail.com>
2017-05-30 12:32:12 -0400

    still working

--- index.js
+++ index.js
@@ -2,9 +2,37 @@
 var http = require("http");
 var fs = require("fs");
 
-var server = http.createServer((req, res)=>{
+// Include socket.io which was installed by npm. It is NOT part of core.
+var socketio = require("socket.io");
+
+var server = http.createServer(function(req, res){
 	console.log("Someone connected via HTTP!");
-})
+	fs.readFile('index.html', 'utf-8',function(error,data){
+		// console.log(error);
+		// console.log(data);
+		if(error){
+			res.writeHead(500,{'content-type':'text/html'});
+			res.end('Internal Server Error');
+		}else{
+			res.writeHead(200,{'content-type':'text/html'});
+			res.end(data);
+		}
+	});
+});
+
+var io = socketio.listen(server);
+// handle socket connections ...
+io.sockets.on('connect',(socket)=>{
+	console.log("Someone connected via socket!");
+	
+	socket.on('nameToServer',(name)=>{
+		console.log(name + " just joined.");		
+		io.sockets.emit('newUser',name);
+	});
+	socket.on('sendMessage',()=>{
+		console.log("Someone clicked on the big blue button.");
+	});
+});
 
 // console.log("The node file is working.")
 server.listen(8080);

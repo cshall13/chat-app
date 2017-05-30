2caf2202ffc07291d9865c283b23cff11054ae70
Shane Hall <cyrus.s.hall84@gmail.com>
2017-05-30 15:56:30 -0400

    added css and date/time

--- index.js
+++ index.js
@@ -7,17 +7,29 @@ var socketio = require("socket.io");
 
 var server = http.createServer(function(req, res){
 	console.log("Someone connected via HTTP!");
-	fs.readFile('index.html', 'utf-8',function(error,data){
-		// console.log(error);
-		// console.log(data);
-		if(error){
-			res.writeHead(500,{'content-type':'text/html'});
-			res.end('Internal Server Error');
-		}else{
-			res.writeHead(200,{'content-type':'text/html'});
-			res.end(data);
-		}
-	});
+	if(req.url == '/'){
+		fs.readFile('index.html', 'utf-8',function(error,data){
+			// console.log(error);
+			// console.log(data);
+			if(error){
+				res.writeHead(500,{'content-type':'text/html'});
+				res.end('Internal Server Error');
+			}else{
+				res.writeHead(200,{'content-type':'text/html'});
+				res.end(data);
+			}
+		});
+	}else if(req.url == '/styles.css'){
+		fs.readFile('styles.css','utf-8',(error,data)=>{
+			if(error){
+				res.writeHead(500,{'content-type':'text/html'});
+				res.end('Internal Server Error');
+			}else{
+				res.writeHead(200,{'content-type':'text/css'});
+				res.end(data);
+			}
+		});
+	}	
 });
 
 var io = socketio.listen(server);
@@ -31,6 +43,10 @@ io.sockets.on('connect',(socket)=>{
 	});
 	socket.on('sendMessage',()=>{
 		console.log("Someone clicked on the big blue button.");
+	});
+	socket.on('messageToServer',(messageObj)=>{
+		var time = (new Date()).toLocaleString();
+		io.sockets.emit('messageToClient',messageObj.name + ' : ' + messageObj.newMessage + ' ' + time);
 	});
 });
 
